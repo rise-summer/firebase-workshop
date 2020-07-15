6ff5acca8ef14b56ad669b49b1741eddb08b815e
milesswu <mileswu13@gmail.com>
2020-07-15 11:30:29 -0700

    Post component
    Created component for generic posts, will be populated by props.

---
+++ final/src/Post.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Post.css";
+
+function Post(props) {
+	const { author, profilePic, text, file } = props;
+
+	return (
+		<div className="Post">
+			<div className="post-header">
+				<p className="post-author">{author}</p>
+			</div>
+			<div className="post-body">
+				<p>{text}</p>
+			</div>
+			<div className="post-details">
+				<i class="fas fa-edit"></i>
+				<i class="fas fa-trash-alt"></i>
+			</div>
+		</div>
+	);
+}
+
+export default Post;
